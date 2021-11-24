dbb16a09bec190a2c93ad32afb0e76f891a37796
Nora Alfstad <57796187+noraalfstad@users.noreply.github.com>
2021-11-24 10:39:10 +0100

    Oppgave 2

--- Tallspill.java
+++ Tallspill.java
@@ -5,36 +5,29 @@ import static javax.swing.JOptionPane.*;
 class Tallspill
 {
     public int nyttTall() {
+        // Trekker og returnerer et slumptall mellom 0 og 200
         int tall = (int) (Math.random() * 201);
         return tall;
-        // Trekker og returnerer et slumptall mellom 0 og 200.
-        // Tips bruk : (int) (Math.random() * 201), som gir verdier mellom 0 og 200
     }
 
     public void visMelding(String melding){
-        String meldingUt = showInputDialog("Jeg tenker på et tall mellom 0 og 200." + "\n" + " Prøv å gjette på tallet.");
-        // Viser parameterens innhold i en meldingsboks.
+        // Viser parameterens innhold i en meldingsboks
+        showMessageDialog(null, melding);
     }
 
     private void forLite(int tall){
-        String meldingForLite = showMessageDialog(null, tall + " er for lite! Prøv igjen!");
-    /* Viser melding om at parameterens verdi er for
-       lite tall og ber spilleren prøve igjen. */
+        //Viser melding om at parameterens verdi er for lite tall og ber spilleren prøve igjen
+        visMelding(tall + " er for lite! Prøv igjen!");
     }
 
     private void forStort(int tall) {
-        String meldingForStort = showMessageDialog(null, tall + " er for stort! Prøv igjen!");
-    /* Viser melding om at parameterens verdi er for
-      stort tall og ber spilleren prøve igjen. */
+        //Viser melding om at parameterens verdi er for stort tall og ber spilleren prøve igjen
+        visMelding(tall + " er for stort! Prøv igjen!");
     }
 
     public void avsluttRunde(int antall, int gjetning) {
-        String meldingAvslutt = showMessageDialog(null, gjetning +
-                " er riktig!" + "\n" + " Du gjettet riktig på " + antall + " forsøk.");
-
-        /* Viser melding om at det ble gjettet riktig
-      og antall gjetninger som ble brukt.
-      Parametrene gir opplysninger om dette. */
+        //Viser melding om at det ble gjettet riktig og antall gjetninger som ble brukt. Parametrene gir opplysninger om dette.
+        visMelding(gjetning + " er riktig!" + "\n" + " Du gjettet riktig på " + antall + " forsøk.");
     }
 
     public void kjørSpill() {
@@ -43,6 +36,40 @@ class Tallspill
       gjetninger fra bruker inntil det gjettes riktig.
       Når det skjer, avsluttes runden med passe
       meldinger til spilleren. */
-        
+        int nyttTall = nyttTall();
+        int teller = 0;
+
+        boolean riktig = false;
+
+        while (riktig == false){
+            teller++;
+
+            String gjetning = showInputDialog("Jeg tenker på et tall mellom 0 og 200." + "\n" + " Prøv å gjette på tallet.");
+
+            //Tester at det skrives inn tall
+            try{
+                Integer gjetningTall = Integer.parseInt(gjetning);
+            }
+            catch (Exception e){
+                Integer gjetningTall = 0;
+                visMelding("Tallet må være et heltall!");
+                System.exit(1);
+            }
+
+            //Definer som tall
+            int gjetningTall = Integer.parseInt(gjetning);
+
+            //Tester om tallet er for stort, for lite eller riktig og avslutter hvis riktig.
+            if (gjetningTall == nyttTall){
+                avsluttRunde(teller, gjetningTall);
+                riktig = true;
+            }
+            else if (gjetningTall < nyttTall){
+                forLite(gjetningTall);
+            }
+            else if (gjetningTall > nyttTall){
+                forStort(gjetningTall);
+            }
+        }
     }
 }
